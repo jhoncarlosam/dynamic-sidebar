2fc2df698f29b1cc2cd6457d7db6cd2eaacb767e
Jhon Carlos Acevedo Mendoza <jhonacevedomendoza@gmail.com>
2021-05-12 19:37:20 -0500

    Añadiendo transición al sidebar

---
+++ script.js
@@ -0,0 +1,10 @@
+let closeButton = document.querySelector('.button-close')
+let menuButton = document.querySelector('.button-menu')
+let sidebar = document.querySelector('.sidebar')
+
+function toggleSideBar(){
+    sidebar.classList.toggle('sidebar--active')
+}
+
+closeButton.addEventListener('click', toggleSideBar)
+menuButton.addEventListener('click', toggleSideBar)
